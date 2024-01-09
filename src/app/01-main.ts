ed0d8503518dc1f89818e24758eb51094182e3e5
htmtmx <cesar.pineda@daksha.mx>
2024-01-09 15:59:29 -0600

    21 - ♻️ Refactor getProducts function to specify axios response type

--- src/app/01-main.ts
+++ src/app/01-main.ts
@@ -4,8 +4,8 @@ import { Product } from './models/product.model';
 
 (async () => {
 
-  async function getProducts(): Promise<Product[]> {
-    const rta = await axios.get('https://api.escuelajs.co/api/v1/products');
+  async function getProducts(){
+    const rta = await axios.get<Product[]>('https://api.escuelajs.co/api/v1/products');
     return rta.data;
   }
 
