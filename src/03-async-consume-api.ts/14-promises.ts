1922986d4ca6d1ac40674a2661ecb076c48ed8e6
htmtmx <cesar.pineda@daksha.mx>
2024-01-09 15:33:35 -0600

    19 - ✨ Add async/await functionality to consume API

--- src/03-async-consume-api.ts/14-promises.ts
+++ src/03-async-consume-api.ts/14-promises.ts
@@ -1,4 +1,6 @@
 //@ts-check
+import axios from 'axios';
+
 (async() => {
   function delay(time: number) {
     const promise = new Promise<boolean>((resolve) => {
@@ -11,4 +13,23 @@
   console.log('---'.repeat(10));
   const rta = await delay(3000);
   console.log(rta);
+
+  function getProducts() {
+    const promise = axios.get('https://api.escuelajs.co/api/v1/products')
+    return promise;
+  }
+
+  // console.log('---'.repeat(10));
+  // const rtaAxios = await getProducts();
+  // console.log(rtaAxios.data);
+
+  async function getProductsAsync() {
+    const rtaAsync = await axios.get('https://api.escuelajs.co/api/v1/products');
+    return rtaAsync.data;
+  }
+
+  console.log('---'.repeat(10));
+  const productsAsync = await getProductsAsync();
+  console.log(productsAsync);
+
 })();
