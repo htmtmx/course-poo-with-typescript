fca62f237dfb5ee59f544ac730f1df95c0f1cb8a
htmtmx <cesar.pineda@daksha.mx>
2024-01-09 16:02:29 -0600

    23 - ♻️ Refactor getProducts function to use destructuring assignment

--- src/app/01-main.ts
+++ src/app/01-main.ts
@@ -5,8 +5,7 @@ import { Product } from './models/product.model';
 (async () => {
 
   async function getProducts(){
-    const rta = await axios.get('https://api.escuelajs.co/api/v1/products');
-    const data = rta.data as Product[];
+    const {data} = await axios.get<Product[]>('https://api.escuelajs.co/api/v1/products');
     return data;
   }
 
