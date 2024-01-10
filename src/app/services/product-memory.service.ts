da28cf90f23cadc6f8912c47d283cff720f53103
htmtmx <cesar.pineda@daksha.mx>
2024-01-10 10:43:40 -0600

    30 - ✨ Add ProductService interface to ProductMemoryService

--- src/app/services/product-memory.service.ts
+++ src/app/services/product-memory.service.ts
@@ -1,9 +1,10 @@
 import { Product } from "./../models/product.model";
 import { CreateProductDto, UpdateProductDto } from "./../dtos/product.dto";
 import { faker } from "@faker-js/faker";
+import { ProductService } from "../models/porduct-service.model";
 
 
-export class ProductMemoryService {
+export class ProductMemoryService implements ProductService {
   private products: Product[] = [];
   create(data: CreateProductDto): Product {
     const newProduct = {
